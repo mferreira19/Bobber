69d28f2a05de231061599072304e6119b8054d9d
kyle <kyle@jefflisa.com>
2018-06-22 11:38:51 -0500

    kyels updated code bang

--- lib/serialhelper.py
+++ lib/serialhelper.py
@@ -3,7 +3,7 @@
 import serial
 from datetime import datetime
 # this port address is for the serial tx/rx pins on the GPIO header
-SERIAL_PORT = '/dev/cu.usbmodem1411'
+SERIAL_PORT = '/dev/cu.usbmodem1421'
 # be sure to set this to the same rate used on the Arduino
 SERIAL_RATE = 9600
 
@@ -18,10 +18,13 @@ def main():
 
     #filetime = (gettime()[0:13])
 
-    print (gettime()[0:13])
+    #print (gettime()[0:13])
 
-#This is a section on creating a cache for the live data display It write
-#It creates a file data.csv writes to an array and then removes the data after 20 lines are full
+    #This is a section on creating a cache for the live data display It write
+    #It creates a file data.csv writes to an array and then removes the data after 20 lines are full
+
+    with open('../HTML/data.csv', "a") as filepointer:
+        filepointer.write("%s,%s,%s\r\n" % ("time", "temp", "hum"))
 
 
 
@@ -34,25 +37,11 @@ def main():
         reading = ser.readline().decode('utf-8')
         parsed_line = reading.strip().split(",")
 
+        # print (parsed_line)
+
         time, temp, humidity = gettime(), parsed_line[0], parsed_line[1]
 
-        print(time, temp, humidity)
-
-
-# this is the File naming system that uses the variable (filetime) to take the Year:Month:Day:Hour and write it to a csv file
-
-
-        #with open("Sensordata" + str(filetime) + ".csv" , "a") as filepointer:
-
-
-        with open('data.csv', "a") as filepointer:
-
-
-
-
-            open("simple" + str(minLength) + str(minBF) + ".csv","w")
-
-
+        with open('../HTML/data.csv', "a") as filepointer:
 
             filepointer.write("%s,%s,%s \r\n" % (time, temp, humidity))
 
